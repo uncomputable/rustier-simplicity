c26c5bfce37316dfa2d7d803772797b45662c8c4
Christian Lewe <ch_lewe@agdsn.me>
2023-04-19 22:19:01 +0200

    Add print_type_of

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod combinator;
 mod display;
 mod error;
 pub mod value;
+
+/// Print type of any given Rust value.
+///
+/// Useful for debugging.
+pub fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
